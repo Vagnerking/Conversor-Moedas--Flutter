783fd30cd942eb14f8f3e668aef3681492e0a105
Vagner Araújo <74484733+Vagnerking@users.noreply.github.com>
2022-12-13 20:47:29 -0300

    20221312_update #ghpages 1

---
+++ linux/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,21 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "/home/vagnerking/snap/flutter/common/flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "/home/vagnerking/Documents/conversor_moedas" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=/home/vagnerking/snap/flutter/common/flutter"
+  "PROJECT_DIR=/home/vagnerking/Documents/conversor_moedas"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=/home/vagnerking/Documents/conversor_moedas/.dart_tool/package_config.json"
+  "FLUTTER_TARGET=/home/vagnerking/Documents/conversor_moedas/lib/main.dart"
+)
